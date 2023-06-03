ecef7f37982c1dbab20502fdf671790cb75c0fb4
sachinsherekar <130824306+sachinsherekar@users.noreply.github.com>
2023-06-03 16:25:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-//your JS code here. If required.
-function updateTextAfterDelay() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve('Hello, world!');
-    }, 1000);
-  });
+// //your JS code here. If required.
+// function updateTextAfterDelay() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve('Hello, world!');
+//     }, 1000);
+//   });
+// }
+
+// // Assuming you have an HTML element with an ID of 'output'
+// const outputElement = document.getElementById('output');
+
+// updateTextAfterDelay().then((message) => {
+//   outputElement.textContent = message;
+// });
+function updateTextAfterDelay(){
+	return new Promise((resolve)=>{
+		setTimeout(()=> {
+			resolve ('Hello,World!');
+		},1000);
+	});
 }
-
-// Assuming you have an HTML element with an ID of 'output'
-const outputElement = document.getElementById('output');
-
-updateTextAfterDelay().then((message) => {
-  outputElement.textContent = message;
-});
+	const outputElement =document.getElementById('output');
+	updateTextAfterDelay().then((message)=>{
+		outputElement.textContent =message;
+	});
