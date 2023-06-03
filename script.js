01fe5313ee6b000c789641faec2f1fb94f8ebe78
sachinsherekar <130824306+sachinsherekar@users.noreply.github.com>
2023-06-03 16:33:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-//your JS code here. If required.
-function updateTextAfterDelay() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve('Hello, world!');
-    }, 1000);
-  });
-}
-
-// Assuming you have an HTML element with an ID of 'output'
-const outputElement = document.getElementById('output');
-
-updateTextAfterDelay().then((message) => {
-  outputElement.textContent = message;
-});
+// //your JS code here. If required.
 // function updateTextAfterDelay() {
-// 	return new Promise((resolve)=>{
-// 		setTimeout(()=> {
-// 			resolve ('Hello, World!');
-// 		},1000);
-// 	});
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve('Hello, world!');
+//     }, 1000);
+//   });
 // }
-// 	const outputElement =document.getElementById('output');
 
-// 	updateTextAfterDelay().then((message) => {
-// 		outputElement.textContent =message;
-// 	});
+// // Assuming you have an HTML element with an ID of 'output'
+// const outputElement = document.getElementById('output');
+
+// updateTextAfterDelay().then((message) => {
+//   outputElement.textContent = message;
+// });
+function updateTextAfterDelay() {
+	return new Promise((resolve)=>{
+		setTimeout(()=> {
+			resolve ('Hello, world!');
+		},1000);
+	});
+}
+	const outputElement =document.getElementById('output');
+
+	updateTextAfterDelay().then((message) => {
+		outputElement.textContent =message;
+	});
