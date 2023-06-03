64601860494f97724e58dc80f45308b8f7c3a880
sachinsherekar <130824306+sachinsherekar@users.noreply.github.com>
2023-06-03 16:15:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function updateTextAfterDelay() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('Hello, world!');
+    }, 1000);
+  });
+}
+
+// Assuming you have an HTML element with an ID of 'output'
+const outputElement = document.getElementById('output');
+
+updateTextAfterDelay().then((message) => {
+  outputElement.textContent = message;
+});
